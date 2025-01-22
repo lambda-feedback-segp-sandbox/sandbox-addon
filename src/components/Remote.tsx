587457d3ca0a2551cd598d62628e1e3f4d8257c9
Manav Sankriti <ms3822@ic.ac.uk>
2025-01-22 18:19:09 +0000

    feat: Form contents of the request to the evaluation function

--- src/components/Remote.tsx
+++ src/components/Remote.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import {Form} from './Form';
-import {useGlobals} from '@storybook/manager-api'
 
 export function Remote() {
-    const [globals, updateGlobals] = useGlobals();
     function remoteEval(url: String, params: JSON) {
         console.log("remote eval");
         console.log(url);
         console.log(params);
+        const answer = "Hello";
         const response = localStorage.getItem('response');
         console.log(response);
+
+        const request = {"response": response, "answer": answer, "params": params};
+
+        console.log(request);
     }
     return (
         <div>
